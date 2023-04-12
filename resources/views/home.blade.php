80d4b07a20402f658409f56a7dddfa36bc7ae2e9
John Doe <ardhiyashita2002@gmail.com>
2023-04-12 23:08:54 +0800

    update not a dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,7 +16,7 @@
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="{{ route('driver_index') }}">Home</a></li>
-              <li class="breadcrumb-item active">Dashboard</li>
+              <li class="breadcrumb-item active">Not A Dashboard</li>
             </ol>
           </div><!-- /.col -->
         </div><!-- /.row -->
